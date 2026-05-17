0e3ea74ba90a99a381a1b5c71a34b9138dc63723
alommamun055-star <alommamun055@gmail.com>
2026-05-17 09:14:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,63 +1,60 @@
 import makeWASocket, {
-useMultiFileAuthState,
-fetchLatestBaileysVersion,
-DisconnectReason
+  useMultiFileAuthState,
+  fetchLatestBaileysVersion,
+  DisconnectReason
 } from "@whiskeysockets/baileys"
 
 import P from "pino"
-import express from "express"
-
-const app = express()
-
-app.get("/", (req, res) => {
-res.send("RELAX-MD Running")
-})
-
-app.listen(9090, () => {
-console.log("Server running on port 9090")
-})
+import http from "http"
 
 async function startBot() {
 
-const { state, saveCreds } =
-await useMultiFileAuthState("./session")
+  // fake web server for render
+  const server = http.createServer((req, res) => {
+    res.writeHead(200)
+    res.end("RELAX-MD Running")
+  })
 
-const { version } =
-await fetchLatestBaileysVersion()
+  server.listen(process.env.PORT || 3000)
 
-const sock = makeWASocket({
-version,
-logger: P({ level: "silent" }),
-auth: state,
-browser: ["RELAX-MD", "Chrome", "1.0.0"],
-syncFullHistory: false
-})
+  const { state, saveCreds } =
+    await useMultiFileAuthState("./session")
 
-sock.ev.on("creds.update", saveCreds)
+  const { version } =
+    await fetchLatestBaileysVersion()
 
-sock.ev.on("connection.update", async ({
-connection,
-lastDisconnect
-}) => {
+  const sock = makeWASocket({
+    version,
+    logger: P({ level: "silent" }),
+    auth: state,
+    browser: ["RELAX-MD", "Chrome", "1.0.0"]
+  })
 
-if (connection === "open") {
+  sock.ev.on("creds.update", saveCreds)
 
-console.log("✅ RELAX-MD Connected")
-console.log("👑 Owner: ⤹𝐗 𝐑𝐎𝐌𝐄𝐎𓂃༊")
+  sock.ev.on("connection.update", async ({
+    connection,
+    lastDisconnect
+  }) => {
 
-}
+    if (connection === "open") {
 
-if (connection === "close") {
+      console.log("✅ RELAX-MD Connected")
+      console.log("👑 Owner Connected")
 
-const shouldReconnect =
-lastDisconnect?.error?.output?.statusCode
-!== DisconnectReason.loggedOut
+    }
 
-if (shouldReconnect) {
-startBot()
-}
-}
-})
+    if (connection === "close") {
+
+      const shouldReconnect =
+        lastDisconnect?.error?.output?.statusCode
+        !== DisconnectReason.loggedOut
+
+      if (shouldReconnect) {
+        startBot()
+      }
+    }
+  })
 }
 
 startBot()
